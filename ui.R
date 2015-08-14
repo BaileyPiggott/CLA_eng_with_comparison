4b5c51e77eebb2e316bb94dad4d17e26b41c8097
Bailey <baileypiggott@gmail.com>
2015-08-14 11:36:03 -0400

    Add description of app to sidebar

--- ui.R
+++ ui.R
@@ -24,7 +24,22 @@ shinyUI(fluidPage(
                 )#end options
     ),#end selectInput
     
-    downloadButton("downloadPDF", "Download PDF")
+    downloadButton("downloadPDF", "Download PDF"),
+    
+    #text description of app ------------------------
+    
+    br(),br(), 
+    h4("About This App"),
+    p("This app displays the CLA+ (Collegiate Learning Assessment Plus) scores of engineering students from each 
+      of the 10 disciplines. CLA+ employs two types of performance assessments: a performance task and a series 
+      of selected-response questions, both of which require students to apply a thought process in order to 
+      arrive at a solution to a problem."),
+    p("First and second year samples are the same students who wrote the CLA test both years."),
+    br(),
+    p("Learn more about the HEQCO Learning Outcomes Assessment Project", 
+      a("here.", href = "http://www.queensu.ca/qloa/"))
+    # end text description 
+    
   ),#end sidebar panel
   
   mainPanel(
